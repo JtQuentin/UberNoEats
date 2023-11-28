673ee52c6d8a3d561789fe8362eae7842d83a624
Yaami Tachi <nicolas.ferreira@efrei.net>
2023-11-28 12:11:25 +0100

    FixMap

--- lib/view/my_map.dart
+++ lib/view/my_map.dart
@@ -5,8 +5,9 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:my_app/controller/my_firestore_helper.dart';
-import 'package:my_app/globale.dart';
 import 'package:my_app/model/my_user.dart';
+
+import '../globale.dart';
 
 class MyMap extends StatefulWidget {
   Position position;
@@ -17,39 +18,49 @@ class MyMap extends StatefulWidget {
 }
 
 class _MyMapState extends State<MyMap> {
+  //variable
   late CameraPosition myPositionMaps;
   Completer<GoogleMapController> control = Completer();
-  Set<Marker> allMarkers = Set();
+  Set<Marker>allMarkers = Set();
+
   @override
   void initState() {
     // TODO: implement initState
-    myPositionMaps = CameraPosition(
-        target: LatLng(widget.position.latitude, widget.position.longitude),
-        zoom: 14);
-    Map<String, dynamic> data = {
-      "GPS": GeoPoint(widget.position.latitude, widget.position.longitude)
+    myPositionMaps = CameraPosition(target: LatLng(widget.position.latitude,widget.position.longitude),zoom: 14);
+    Map<String,dynamic> data = {
+      "GPS":GeoPoint(widget.position.latitude,widget.position.longitude)
     };
     MyFirestoreHelper().updateUser(moi.uid, data);
+    //récuperer les élements dans la base de donneé
     MyFirestoreHelper().cloudUsers.snapshots().listen((event) {
-      setState(() {});
-      List documents = event.docs;
-      for (int i in documents) {
-        MyUser lesautres = MyUser.dataBase(documents[i]);
-        if (moi.uid != lesautres.uid) {
-          if (GeoPoint(0, 0) != lesautres.gps) {
-            Marker marker = Marker(
+      setState(() {
+        List documents = event.docs;
+        for(int i= 0;i<documents.length;i++){
+          MyUser lesautres = MyUser.dataBase(documents[i]);
+          if(moi.uid != lesautres.uid) {
+            if(GeoPoint(0,0)!= lesautres.gps!) {
+              Marker marker = Marker(
                 markerId: MarkerId(lesautres.uid),
-                position:
-                    LatLng(lesautres.gps!.latitude, lesautres.gps!.longitude),
-                infoWindow: InfoWindow(title: lesautres.fullName));
-            allMarkers.add(marker);
+                position: LatLng(
+                    lesautres.gps!.latitude, lesautres.gps!.longitude),
+                infoWindow: InfoWindow(
+                  title: lesautres.fullName,
+                ),
+
+
+
+
+              );
+              allMarkers.add(marker);
+            }
           }
+
         }
-      }
+      });
+
     });
     super.initState();
   }
-
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
@@ -58,10 +69,10 @@ class _MyMapState extends State<MyMap> {
       myLocationEnabled: true,
       myLocationButtonEnabled: true,
       onMapCreated: (controller) async {
-        String newStyle = await DefaultAssetBundle.of(context)
-            .loadString("lib/styleMaps.json");
+        String newStyle = await DefaultAssetBundle.of(context).loadString("lib/styleMaps.json");
         controller.setMapStyle(newStyle);
         control.complete(controller);
+
       },
     );
   }
